e22d02991c6c3031d0a3c572b61ceeba5f3dd825
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2020-06-13 18:02:59 -0500

    Updating Product

--- online-shopping-backend/models/product.js
+++ online-shopping-backend/models/product.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
     description: {
         type: String,
         required: true
